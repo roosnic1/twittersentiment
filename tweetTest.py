7e6bd73db30c2a358f32cadecfc3b062a1310d57
Nicolas Roos <nicolas.k.roos@gmail.com>
2014-05-25 16:45:45 +0200

    added loading of csv file

--- tweetTest.py
+++ tweetTest.py
@@ -1,34 +1,31 @@
-# import tweepy
-
-# auth = tweepy.OAuthHandler('TFJ4Kkr9ua2N7622SIWgQIOWG','z42OahWDlMBybxkEP4DbPYm0tlsnXhf6nU7xjdjRQ2VVpQIf2S')
-# auth.set_access_token('2375675226-ajmqtZ6TdJ5kCgUfrIdcGFPoKSe1xLpbhEFphZb','YyO6sbAi7No0xwGnpQCS4Noy6HA6zYtK3mJzWesb8rz4a')
-
-# api = tweepy.API(auth)
-
-# #public_tweets = api.home_timeline()
-# #for tweet in public_tweets:
-# #	print tweet.text
-
-# #user = api.get_user('Layzapp')
-
-# #print user.screen_name
-# #print user.followers_count
-# #for friend in user.friends():
-# #	print friend.screen_name
-
-# search_results = api.search('layzapp',rpp=100)
-
-# print search_results
-# for s in search_results:
-# 	print s.text
-
 
 
 from twython import Twython
+import csv
 
-twitter= Twython('TFJ4Kkr9ua2N7622SIWgQIOWG','z42OahWDlMBybxkEP4DbPYm0tlsnXhf6nU7xjdjRQ2VVpQIf2S','2375675226-ajmqtZ6TdJ5kCgUfrIdcGFPoKSe1xLpbhEFphZb','YyO6sbAi7No0xwGnpQCS4Noy6HA6zYtK3mJzWesb8rz4a')
 
-search_results = twitter.search(q='layzapp',until='2014-04-13',count=100)
+def searchTweets(query):
+	twitter= Twython('TFJ4Kkr9ua2N7622SIWgQIOWG','z42OahWDlMBybxkEP4DbPYm0tlsnXhf6nU7xjdjRQ2VVpQIf2S','2375675226-ajmqtZ6TdJ5kCgUfrIdcGFPoKSe1xLpbhEFphZb','YyO6sbAi7No0xwGnpQCS4Noy6HA6zYtK3mJzWesb8rz4a')
+	search_results = twitter.search(q=query,count=100)
 
-for q in search_results['statuses']:
-	print q['text']
+	for q in search_results['statuses']:
+		print q['text']
+
+
+def loadTwitterCSV(file):
+	print file
+	tweetList = []
+	with open(file,'r') as csvfile:
+		tweets = csv.reader(csvfile,delimiter=',')
+		for row in tweets:
+			tweetList.append({'polarity':row[0],'id':row[1],'date':[2],'query':[3],'user':[4],'text':[5]})
+	print len(tweetList)
+	return tweetList
+
+
+
+if __name__ == "__main__":
+	#searchTweets('layzapp')
+	tweetlist = loadTwitterCSV('trainingandtestdata/testdata.csv')
+
+
