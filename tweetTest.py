431a5372edfd449eae7742a3c1b9c36f2a7112d8
Nicolas Roos <nicolas.k.roos@gmail.com>
2014-05-26 13:36:29 +0200

    Added filtering methode

--- tweetTest.py
+++ tweetTest.py
@@ -13,19 +13,40 @@ def searchTweets(query):
 
 
 def loadTwitterCSV(file):
-	print file
+	#print file
 	tweetList = []
 	with open(file,'r') as csvfile:
 		tweets = csv.reader(csvfile,delimiter=',')
 		for row in tweets:
-			tweetList.append({'polarity':row[0],'id':row[1],'date':[2],'query':[3],'user':[4],'text':[5]})
-	print len(tweetList)
+			tweetList.append({'polarity':row[0],'id':row[1],'date':row[2],'query':row[3],'user':row[4],'text':row[5]})
+	#print len(tweetList)
 	return tweetList
+
+def filterTweets(tweetList,query):
+	filterList = []
+	if query.find('@') == 0:
+		for tweet in tweetlist:
+			#print tweet
+			if tweet['user'] == query[1:]:
+				filterList.append(tweet)
+			elif tweet['text'].find(query) >= 0:
+				filterList.append(tweet)
+	else:
+		for tweet in tweetlist:
+			if tweet['text'].lower().find(query.lower()) >= 0:
+				filterList.append(tweet)
+
+	return filterList
+
 
 
 
 if __name__ == "__main__":
 	#searchTweets('layzapp')
 	tweetlist = loadTwitterCSV('trainingandtestdata/testdata.csv')
+	print 'Search for user:'
+	print filterTweets(tweetlist,'@sulu34')
+	print 'Search for query:'
+	print filterTweets(tweetlist,'obama')
 
 
